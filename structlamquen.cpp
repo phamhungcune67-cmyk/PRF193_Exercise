d8c3cbe180711b0fdd5260ce5196449b23b20226
phamhungcune67-cmyk <phamhungcune67@gmail.com>
2025-10-21 09:43:42 +0700

    Add files via upload

--- structlamquen.cpp
+++ structlamquen.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 using namespace std;
-struct student{
+class Student{
+
+private:
 	string name;
 	int age;
-};
-int main(){
-	student st;
-	cout<<"Nhap ho ten:";
-	getline(cin, st.name);
-	cout<<"Nhap so tuoi";
-	cin>>st.age;
-	cout<<"Name: "<<st.name<<endl;
-	cout<<"Age: "<<st.age<<endl;
-	
-	return 0;
+public:
+	void getinformation(){
+		cin>>name;
+		cin>>age;
+	}
+	void display(){
+		cout<<"Name: "<<name<<endl;
+		cout<<"Age: "<<age<<endl;
 }
+};
+	int main(){
+		Student S;
+		S.getinformation();
+		S.display();
+		return 0;
+	}
+	
