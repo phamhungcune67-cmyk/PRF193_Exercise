01acd6d74cf8254feee2a98742b2d826109c9f74
phamhungcune67-cmyk <phamhungcune67@gmail.com>
2025-10-31 09:45:48 +0700

    Add files via upload

---
+++ chuinhoachuinthuong.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string s;
+	getline(cin, s);
+	for(int i=0;i<s.length();i++){
+		if(s[i] >= 'A' && s[i] <= 'Z') {
+	s[i]=s[i]+32;
+	}
+
+}
+	cout<<s;
+	return 0;
+}
